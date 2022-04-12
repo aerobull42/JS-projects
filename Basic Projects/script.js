cc67e4e470baae5d97967aa9b2caa3b9dbd3c0fc
aerobull42 <doshinaman2006@gmail.com>
2022-04-12 17:45:08 +1000

    added assignments

---
+++ Basic Projects/script.js
@@ -0,0 +1,38 @@
+function AssignmentOne() {
+  let country = "Australia";
+  let continent = "Oceania";
+  let population = 25.5;
+  console.log(country, continent, population);
+}
+
+function AssignmentTwo() {
+  let country = "Australia";
+  let continent = "Oceania";
+  let population = 25.5;
+  let isIsland = true;
+  let language;
+  console.log(
+    typeof country,
+    typeof continent,
+    typeof population,
+    typeof isIsland,
+    typeof language
+  );
+}
+
+function AssignmentThree() {
+  const country = "Australia";
+  const continent = "Oceania";
+  let population = 25.5;
+  const isIsland = true;
+  const language = "English";
+  console.log(
+    typeof country,
+    typeof continent,
+    typeof population,
+    typeof isIsland,
+    typeof language
+  );
+}
+
+AssignmentThree();
